c247e04477881ef40a05a856969d9c4c6d8a50ad
chakkrapan <chakkrapan.r@amadothailand.co.th>
2024-09-18 18:53:40 +0700

    leaning

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
   <section>
-    <img :src=picture :width="size" :height="size"/><br>
-    ป้อนชื่อเล่น: <input type="text"/>
-    <h1>ชื่อ: {{ getFullName() }}</h1>
+    <form></form>
+    <img :src=picture :width="size" :height="size" ref="imageEL" /><br>
+
+    <h1>ชื่อ: {{ getFullName }}</h1>
     <h1>อายุ: {{ age }} ปี</h1>
-    <a>ที่อยู่: <span v-html="address"></span></a>
-    <p>Social: <a :href="social" target="_blank">facebook</a></p>
-    <p>งานอดิเรก: </p>
-    <ul>
-      <li>{{ hobby[0] }}</li>
-      <li>{{ hobby[1] }}</li>
-      <li>{{ hobby[2] }}</li>
-    </ul>
-    <p>ข้อมูลพื้นฐาน</p>
-    <ul>
-      <li>เพศ: {{ general.gender }}</li>
-      <li>น้ำหนัก: {{ general.height }} KG</li>
-      <li>ส่วนสูง: {{ general.weight }} CM</li>
-      <li>โรคประจำตัว: {{ general.status }}</li>
-    </ul>
-    <button @click="showData()">คลิกเพื่อดูข้อมูล</button>
-    <button @click="incressment(10)">เพิ่ม</button>
-    <button @click="decressment(10)">ลด</button>
+    <h1>เงินเดือน: {{ salary }} บาท</h1>
+    <h1>รายได้ต่อปี: {{ getIncome }} บาท</h1>
+    <h1>ตำแหน่ง: {{ getDepartment }}</h1>
+    <button @click="addSalary(5000)">เพิ่มเงินเดือน</button>
+
+    <button @click="toggleVisible">{{ isVisible ? "ซ่อน" : "แสดง" }}รายละเอียด</button>
+    <article v-show="isVisible">
+      <a>ที่อยู่: <span v-html="address"></span></a>
+      <p>Social: <a :href="social" target="_blank">facebook</a></p>
+      <p v-if="hobby.length === 0">ไม่มีงานอดิเรก</p>
+      <div v-else>
+        <p>งานอดิเรก: </p>
+        <ul>
+          <li v-for="(item, index) in hobby" :key="index"> {{ index + 1 }}. {{ item }} </li>
+        </ul>
+      </div>
+      <p>ข้อมูลพื้นฐาน</p>
+      <ul>
+        <li v-for="(item, key) in general" :key="key"> {{ key }}. {{ item }} </li>
+      </ul>
+    </article>
   </section>
 </template>
 
@@ -29,7 +33,7 @@
 export default {
   name: 'App',
   data() {
-    return{
+    return {
       firstname: "PPP",
       lastname: "zzz",
       age: 20,
@@ -37,31 +41,47 @@ export default {
       picture: "https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/corporate-user-icon.png",
       size: 150,
       social: "https://ppppp.com",
-      hobby: [
-        "ทำสวน", "เล่นเกม", "ฟังเพลง"
-      ],
+      hobby: ["ssss", "11111", "2222", "33333", "2345"],
       general: {
-        gender: "ชาย", 
-        weight: 80, 
-        height: 170, 
+        gender: "ชาย",
+        weight: 80,
+        height: 170,
         status: false
-      }
+      },
+      isVisible: false,
+      salary: 25000
     }
   },
   methods: {
-    getFullName(){
+    toggleVisible() {
+      this.isVisible = !this.isVisible;
+    },
+    addSalary(value) {
+      this.salary += value;
+    }
+  },
+  computed:{
+    getFullName() {
       return this.firstname + " " + this.lastname
     },
-    showData(){
-      alert(this.firstname)
+    getIncome(){
+      return this.salary * 12;
     },
-    incressment(value){
-      this.age += value;
-    },
-    decressment(value){
-      this.age -= value;
+    getDepartment() {
+      return this.salary>=35000? "Project Manager": "Programmer";
+    }
+  },
+  watch:{
+    salary(value) {
+      if(value > 50000){
+        alert("เงินเดือนไม่ควรเกิน 50000 บาท");
+        setTimeout(()=>{
+          this.salary = 20000;
+        }, 2000)
+      }
     }
   }
+
 }
 </script>
 
